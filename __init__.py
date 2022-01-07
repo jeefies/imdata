d750198d8a63b59419f8f1c4fcbd679cd85badcb
jeefies <jeefy163@163.com>
2022-01-07 21:00:03 +0800

    update to a direct use lib

--- __init__.py
+++ __init__.py
@@ -16,22 +16,19 @@ class ImData:
     format = 'bmp'
 
     def __init__(self, content = None):
-        if content:
-            self.content = content
-            self.imdata = self.bytes_pix(content)
+        self.content = content
 
-    def save(self, filename = None, content = None):
+    def save(self, filename = None, content = None, **kwargs):
         """
         filename can be str, pathlib.Path or file object
+        kwargs is for bytes_pix that will use in this function
+        self.content will update to content given after using this
         """
-        ctt = content if content else self.content
+        self.content = ctt = content if content else self.content
         if not ctt:
             raise TypeError("no content given in")
 
-        if self.imdata:
-            return imageio.imsave(filename, self.imdata, format=self.format)
-        else:
-            return imageio.imsave(filename, self.bytes_pix(ctt), format=self.format)
+        return imageio.imsave(filename, self.bytes_pix(ctt, **kwargs), format=self.format)
 
     def read(self, filename = None):
         """
@@ -41,6 +38,12 @@ class ImData:
 
         self.imdata = imageio.imread(filename, format=self.format)
         self.content = self.pix_bytes(self.imdata)
+
+    @property
+    def data(self):
+        if self.content is not None:
+            return self.bytes_pix(self.content)
+        return None
 
 
     @staticmethod
@@ -76,35 +79,52 @@ class ImData:
                 except:
                     raise TypeError("Not a right object to change into string") # final error
 
-    def bytes_pix(self, bs = None, size = None, encode='utf-8', matrix = None):
+    def bytes_pix(self, bs = None, size = None, encode='utf-8', matrix = None, matrix_compress = False):
         """
         turn bytes into pixel info
         
         Args:
             bs = None: The bytes or the string you wish to turn to imdata
-            size = None: you can set the size your self, or it will use _autosize to get the size from length 
-            matrix = None: the list or list-like object (get content by index), if it's given, bs is no use anymore
+            size = None: 
+                you can set the size your self, or it will use _autosize to get the size from length 
+                Notice that there's 5 place should be remained for info
+            matrix = None: 
+                the list or list-like object (get content by index), if it's given, bs is no use anymore
+                remember every number should smaller than 256 (exculde 256)
         """
         if not matrix:
             base = base64.b85encode(zlib.compress(self.to_bytes(bs, encode))) # change to base85 string, only use ascii letters
+            imtype = 0
         else:
             base = matrix
+            imtype = 1
+            if matrix_compress:
+                base = zlib.compress(bytes(matrix))
+                imtype = 2
 
         if size:
             size = (*size, 3) # get the im size
             length = self._size(size) # get max data length
-            if len(base) > length:
+            if len(base) + 5 > length:
                 raise SizeError('size not enough for data, please use bigger size.')
         else:
-            size = self._autosize(len(base))
+            size = self._autosize(len(base) + 5)
             length =  self._size(size)
         im = np.zeros(length, dtype='uint8') # create lear data
 
 
         # predata, has a chuck data place
-        # 1 for type, 4 for size
-        # type 0 for  bs, type 1 for plain matrix
-        im[0] =  1 if matrix else 0
+        # 数据头，记录数据大小
+        # 1 place (8 bits) for type, 4 place (32 bits) for size
+        # 8 bits 记录数据类型，32 bits 记录数据长度
+        # 8 bits 占一位
+        # type 0 for bs, type 1 for plain matrix, type 2 for bs matrix
+        # 类型0 为单纯的字符数据，类型1 是数组数据，类型2 是压缩过后的数组数据
+        # type 3 for chencode, it's an extra lib
+        # more see github.com/jeefies/chencode main.py function to_imdata_pixels
+        # 类型3 是checode里的一中加密方式，是一个额外的库(也是我写的^_^)
+        # 详见 github.com/jeefies/chencode main.py to_imdata_pixels
+        im[0] =  imtype
         content_length = len(base)
         for i in range(4):
             im[1 + i] = content_length & (0xff << (i * 8))
@@ -127,19 +147,29 @@ class ImData:
         """
         turn pixels into bytes or matrix data
         notice the matrix data would turn to numpy.array type
+        plain text data would turn to bytes
+        把图像数据转为实际数据，
         """
+        # 将三维图像数组转为一维
         row = pix.reshape(-1)
-        rtype = row[0]
+        # 获取图像类型，字符串，还是字节数组
+        imtype = row[0]
         
+        # 获取余下的信息
         length = 0
         for i in range(4):
             length |= row[1 + i] << (i * 8)
 
-        if rtype == 1:
+        if imtype == 1:
+            # 数组就直接返回
             return row[5 : length + 5]
+        elif imtype == 2:
+            ctx = zlib.decompress(bytes(row[5 : length + 5]))
+            return np.array(ctx, dtype="uint8")
         else:
-            stri = ''.join(chr(code) for code in row[5 : length + 5])
-            return zlib.decompress(base64.b85decode(self.to_bytes(stri)))
+            # 字符串拼接只后会返回bytes
+            stri = bytes(row[5 : length + 5])
+            return zlib.decompress(base64.b85decode(stri))
     
     @staticmethod
     def _autosize(length, w = 4, h = 3):
@@ -155,7 +185,7 @@ class ImData:
         # 两者应相等，这样才能保证两者比不变
         cell = math.sqrt(m)
         size = (cell * w, cell * h, 3)
-        # 在这里取整，可以减少一点误差，但不能完全保证横竖比
+        # 在这里取整，可以减少一点误差，但不能完全保证绝对横竖比
         return tuple(math.ceil(i) for i in size)
 
 class SizeError(Exception): pass
--- test.py
+++ test.py
@@ -1,22 +1,53 @@
 from imdata import ImData, SizeError
 
-imd = ImData()
+# init with data
+# 初始化的数据
+imd = ImData("origin things here")
 
 matrix = list(range(100))
-ctx = "fuck it bitch" * 20
+ctx = "hello world " * 20
 
-matrix_dt = imd.bytes_pix(matrix=matrix)
+# 三种方式转换为像素数组
+# three ways to convert to digital data
+# 第一种，将字符串，字节，经压缩转换
+# first one, convert string, bytes, .etc with compress
 ctx_dt = imd.bytes_pix(ctx)
+# 第二种，将数组信息直接转为图像信息
+# 注意，数组里不能出现大于等于256的数字，且只能为整数
+# second, convert num array directly into imdata
+# notice, there shouldn't be any number more than 256 (256 included), and every one should be a int
+matrix_dt = imd.bytes_pix(matrix=matrix)
+# 第三种，将数组信息压缩后转换
+# third, compress the array data and convert into imdata
+cmatrix_dt = imd.bytes_pix(matrix=matrix, matrix_compress=True)
 
-
+# 还原数据，注意返回的数据类型
+# 数组会返回 numpy.ndarray, 字符会返回 bytes
+# get the original data, notice the type returned
+# matrix would turn into numpy.ndarray array, string or bytes data would turn into bytes data
 matrix_o = imd.pix_bytes(matrix_dt)
 ctx_o = imd.pix_bytes(ctx_dt)
+cmatrix_o = imd.pix_bytes(matrix_dt)
 
-print(matrix_o, ctx_o, sep='\n')
+print(matrix_o, ctx_o, cmatrix_o, sep='\n')
+# imd.data是一个property，每次使用都会重新生成图像像素数据
+# imd.data is a property, every time use would generate the image data according to imdata.content
+# that means use it often can lower the speed of the program!
+print("imdata's data:\n", imd.data)
 
+# the way to save or read a image's data
 with open("test.bmp", 'wb') as f:
     imd.save(f, ctx)
+# the same as
+# imd.save("test.bmp", ctx)
+# also
+# imd = ImData(ctx)
+# imd.save("test.bmp")
 
-with open('test.bmp', 'rb') as f:
+with open("test.bmp", 'rb')  as f:
     imd.read(f)
-    print(imd.content)
+# the same as
+# imd.read("test.bmp")
+
+# result save in imd.content
+print(imd.content)
